1591ec7d49ea0292a09433c9f79231690ac18cb4
blum <yuuybonito@gmail.com>
2022-12-25 17:16:30 -0300

    more changes

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps size between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 0;        /* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -71,6 +71,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      spawn,          {.v = brwscmd } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = disccmd } },
 	{ MODKEY,                       XK_s,      spawn,          {.v = spotcmd } },
+	//{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = scr3cmd } },
+	//{ MODKEY|ControlMask,           XK_s,      spawn,          {.v = scr2cmd } },
+	//{ MODKEY|ShiftMask|ControlMask, XK_s,      spawn,          {.v = scr1cmd } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
